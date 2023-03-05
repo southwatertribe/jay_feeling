cd1205d4147ef1b3c40f9637ceea270319d6a46d
BlueJayQ17 <114033915+BlueJayQ17@users.noreply.github.com>
2023-03-04 17:20:39 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:jay_feeling/HomePage.dart';
+=======
+>>>>>>> Stashed changes
 
 import 'package:jay_feeling/LoginPage.dart';
 
@@ -22,18 +25,13 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.lightGreen,
       ),
+<<<<<<< Updated upstream
       home: const HomePage(),
+=======
+      home: const LoginPage()
+>>>>>>> Stashed changes
     );
   }
 }
